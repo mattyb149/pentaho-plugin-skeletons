a7d03f32b1b429a87af9822504ed794f71c9eb2f
Matt Burgess <mburgess@pentaho.com>
2013-07-18 09:23:07 -0400

    Initial commit for PDI step plugin skeleton

---
+++ pdi-step-plugin-skeleton/src/org/pentaho/di/trans/steps/mytrans/MyTrans.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2012 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.trans.steps.mytrans;
+
+import org.pentaho.di.core.annotations.Step;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.i18n.BaseMessages;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.BaseStep;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+
+/**
+ * Describe your step plugin.
+ * 
+ */
+@Step(id = "MyStepId", image = "MyStepIcon.png", name = "My Step Name")
+public class MyTrans extends BaseStep implements StepInterface
+{
+	private static Class<?> PKG = MyTransMeta.class; // for i18n purposes, needed by Translator2!!   $NON-NLS-1$
+	
+	public MyTrans(StepMeta stepMeta, StepDataInterface stepDataInterface, int copyNr, TransMeta transMeta, Trans trans)
+	{
+		super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
+	}
+	
+	public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
+	{
+		Object[] r=getRow();    // get row, set busy!
+		if (r==null)  // no more input to be expected...
+		{
+			setOutputDone();
+			return false;
+		}
+		
+		putRow(getInputRowMeta(), r);     // copy row to possible alternate rowset(s).
+
+        if (checkFeedback(getLinesRead())) 
+        {
+        	if(log.isBasic()) logBasic(BaseMessages.getString(PKG, "DummyTrans.Log.LineNumber")+getLinesRead()); 
+        }
+			
+		return true;
+	}
+}
